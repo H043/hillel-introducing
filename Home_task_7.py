a2f407600dd4c642ec2404c3bce150f8ba6a25dd
H043 <74686459+H043@users.noreply.github.com>
2020-12-06 23:03:37 +0100

    start 7

---
+++ Home_task_7.py
@@ -0,0 +1,11 @@
+# import random
+# my_rand = [random.randint(0, 101) for i in range(20)]
+# print(my_rand)
+#########################################
+my_str = "I am the string"
+a = ''
+
+def PRINTING():
+
+if my_str == str:
+    PRINTING()
