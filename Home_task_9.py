0ad3e9e98a96f9af22e05587bfc7256d553eb1fe
H043 <74686459+H043@users.noreply.github.com>
2020-12-21 01:10:48 +0200

    home task 9 ready

---
+++ Home_task_9.py
@@ -0,0 +1,74 @@
+import random
+from random import randint
+
+def generate_string(min_limit=100, max_limit=1000):
+    str_len = random.randint(min_limit, max_limit)
+    r_list = [chr(random.randint(ord("a"), ord("z"))) for _ in range(str_len)]
+    return ''.join(r_list)
+
+
+def letter_to_space(str_to_transform):
+    count_space = len(str_to_transform) // 5
+    space_index = []
+    while len(space_index) < count_space:
+        index = random.randint(5, len(str_to_transform) - 5)
+        if index not in space_index:
+            space_index.append(index)
+    for index in space_index:
+        str_to_transform = str_to_transform[:index] + " " + str_to_transform[index + 1:]
+    return str_to_transform
+
+# def change_space_to_newline(str_to_transform):
+#     count_new_line = len(str_to_transform) // 3
+#     new_line_indexes = []
+#     while len(new_line_indexes) < count_new_line:
+#         index = random.randint(10, len(str_to_transform) - 10)
+#         if index not in new_line_indexes:
+#             new_line_indexes.append(index)
+#     for index in new_line_indexes:
+#         str_to_transform = str_to_transform[:index] + "\n" + str_to_transform[index + 1:]
+
+
+def change_first_symbol(word):
+    return word.capitalize()
+
+def change_last_symbol(word):
+    punctuation = ".,!?\n"
+    word = word[:-1] + random.choice(punctuation) if len(word) > 3 else word
+    return word
+
+def change_word_to_number(word):
+    if len(word) <= 3:
+        number_list = [str(random.randint(0, 9)) for _ in range(len(word))]
+        word = "".join(number_list)
+    return word
+
+
+
+
+
+
+def random_word_transformation(word):
+    state = random.randint(1, 6)
+    if state == 1:
+        word = change_first_symbol(word)
+    if state == 2:
+        word = change_last_symbol(word)
+    if state == 3:
+        word = change_word_to_number(word)
+    return word
+
+
+def transform_by_words(str_to_transform):
+    words = str_to_transform.split()
+    new_words = []
+    for word in words:
+        new_word = random_word_transformation(word)
+        new_words.append(new_word)
+    return " ".join(new_words)
+
+
+res = generate_string()
+res = letter_to_space(res)
+res = transform_by_words(res)
+print(res)
--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import random
+from random import randint
+
 # my_list = [1, 2, 3, 4]
 # for i in range(1, len(my_list), 1):
 #     my_list[i-1], my_list[i] = my_list [i], my_list[i-1]
@@ -17,33 +19,47 @@ import random
 # ip_list_1 = ["23.123.45.1", "23.123.45.107", "23.124.45.107"]
 # ip_list_2 = ["23.123.45.10", "23.123.45.107", "23.124.45.17"]
 #
-# ip_set_1 = set(ip_list_1)
-# ip_set_2 = set(ip_list_2)
+# # ip_set_1 = set(ip_list_1)
+# # ip_set_2 = set(ip_list_2)
+# #
+# # ip_intersect = ip_set_1.intersection(ip_set_2)
+# # ip_union = ip_set_1.union(ip_set_2)
+# # # ip_set_1.update(ip_set_2)
+# #
+# # print(ip_set_1)
+# # print(ip_set_2)
+# # print(ip_intersect)
+# # print(ip_union)
 #
-# ip_intersect = ip_set_1.intersection(ip_set_2)
-# ip_union = ip_set_1.union(ip_set_2)
-# # ip_set_1.update(ip_set_2)
 #
-# print(ip_set_1)
-# print(ip_set_2)
-# print(ip_intersect)
-# print(ip_union)
+#
+#
+# alphabet_ = [chr(index) for index in range(ord("A"), ord("Z"))]
+# # r_text_len = (random.randint(5, 7))
+# # r_sec_name = []
+# # r_numb = (random.randint(100, 999))
+# # r_fam = ''.join(random.choice(open_family_name()))
+# # for i in alphabet_:
+# #     if len(r_sec_name) != r_text_len:
+# #         r_sec_name.append(random.choice(alphabet_))
+# # sec_name = ''.join(r_sec_name)
+# # with open("domains.txt", "r") as file:
+# #     domain_list2 = []
+# #     for line in file.readlines():
+# #         domain_list2.append(line.replace(".", "").strip())
+# # r_domain = ''.join(random.choice(domain_list2))
+# print(alphabet_)
+a = "ds jhf sdf sdf sadf sadgr3q s ag sad 3 d"
 
 
+def change_space_to_newline(a):
+    count_new_line = len(a) // 3
+    new_line_indexes = []
+    while len(new_line_indexes) < count_new_line:
+        index = random.randint(10, len(a) - 10)
+        if index not in new_line_indexes:
+            new_line_indexes.append(index)
+    for index in new_line_indexes:
+        a = a[:index] + "\n" + a[index + 1:]
 
-
-alphabet_ = [chr(index) for index in range(ord("A"), ord("Z"))]
-# r_text_len = (random.randint(5, 7))
-# r_sec_name = []
-# r_numb = (random.randint(100, 999))
-# r_fam = ''.join(random.choice(open_family_name()))
-# for i in alphabet_:
-#     if len(r_sec_name) != r_text_len:
-#         r_sec_name.append(random.choice(alphabet_))
-# sec_name = ''.join(r_sec_name)
-# with open("domains.txt", "r") as file:
-#     domain_list2 = []
-#     for line in file.readlines():
-#         domain_list2.append(line.replace(".", "").strip())
-# r_domain = ''.join(random.choice(domain_list2))
-print(alphabet_)
+print(change_space_to_newline(a))
