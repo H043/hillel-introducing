603446a3b8c430674ec1ed282796cdedfb5f7d71
H043 <74686459+H043@users.noreply.github.com>
2020-12-20 21:49:36 +0200

    home task 8 ready

--- home_task_8.py
+++ home_task_8.py
@@ -1,37 +1,40 @@
 import os
 import random
-domain_list = []
+from random import randint
+from typing import List
+
+
 def open_domain_file():
     with open("domains.txt", "r") as file:
         domain_list = []
         for line in file.readlines():
             domain_list.append(line.replace(".", "").strip())
     return domain_list
-# print(open_domain_file())
 #########################################
+def open_family_name():
+    with open("Names.txt", "r") as family_file:
+        family_names = []
+        for line in family_file.readlines():
+            family_names.append(line.split()[1])
+    return family_names
+#########################################
+def random_domain():
+    return random.choice(open_domain_file())
 
-with open("Names.txt", "r") as file:
-    family_name = []
-    for line in file.readlines():
-        family_name.append(line.split()[1])
-# print(family_name)
-#########################################
+
+def random_name():
+    return random.choice(open_family_name())
+
+
+def random_number():
+    return random.randint(100, 1000)
+
+
+def secondary_text():
+    return ''.join(chr(randint(ord("a"), ord("y"))) for i in range(randint(5, 7)))
+
 
 def mail_generator():
-   new_mail = ''.join(str(r_fam) + str(r_numb) + "@" + sec_name + "." + r_domain)
-   return new_mail
-alphabet_ = [chr(index) for index in range(ord("A"), ord("Z")+1)]
-r_text_len = (random.randint(5, 7))
-r_sec_name = []
-r_numb = (random.randint(0, 999))
-r_fam = ''.join(random.choice(family_name))
-for i in alphabet_:
-    if len(r_sec_name) != r_text_len:
-        r_sec_name.append(random.choice(alphabet_))
-sec_name = ''.join(r_sec_name)
-with open("domains.txt", "r") as file:
-    domain_list2 = []
-    for line in file.readlines():
-        domain_list2.append(line.replace(".", "").strip())
-r_domain = ''.join(random.choice(domain_list2))
-print(mail_generator())
+    # new_mail = ''.join(str(r_fam) + str(r_numb) + "@" + sec_name + "." + r_domain)
+    print(random_name() + "." + str(random_number()) + "@" + secondary_text() + "." + str(random_domain()))
+mail_generator()
--- test.py
+++ test.py
@@ -1,12 +1,12 @@
-#
+import random
 # my_list = [1, 2, 3, 4]
 # for i in range(1, len(my_list), 1):
 #     my_list[i-1], my_list[i] = my_list [i], my_list[i-1]
 #     print(my_list)
-# # print(my_list)
-example_string = 'aaasssdd`'
-my_random = ''.join(list(set(example_string)))
-print(my_random)
+# # # print(my_list)
+# example_string = 'aaasssdd`'
+# my_random = ''.join(list(set(example_string)))
+# print(my_random)
 
 # my_list = ["3", "6", "2", 8, 1, 9]
 # my_list = list(set(my_list))
@@ -28,3 +28,22 @@ print(my_random)
 # print(ip_set_2)
 # print(ip_intersect)
 # print(ip_union)
+
+
+
+
+alphabet_ = [chr(index) for index in range(ord("A"), ord("Z"))]
+# r_text_len = (random.randint(5, 7))
+# r_sec_name = []
+# r_numb = (random.randint(100, 999))
+# r_fam = ''.join(random.choice(open_family_name()))
+# for i in alphabet_:
+#     if len(r_sec_name) != r_text_len:
+#         r_sec_name.append(random.choice(alphabet_))
+# sec_name = ''.join(r_sec_name)
+# with open("domains.txt", "r") as file:
+#     domain_list2 = []
+#     for line in file.readlines():
+#         domain_list2.append(line.replace(".", "").strip())
+# r_domain = ''.join(random.choice(domain_list2))
+print(alphabet_)
