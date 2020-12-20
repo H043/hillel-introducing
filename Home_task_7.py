1049a61cd8f54db414621d61f29feecd3a517b0c
H043 <74686459+H043@users.noreply.github.com>
2020-12-20 16:53:25 +0200

    home task 7 finally

--- Home_task_7.py
+++ Home_task_7.py
@@ -1,32 +1,50 @@
-# import random
-# # my_rand = [random.randint(0, 101) for i in range(20)]
-# # print(my_rand)
-# # ########################################
-#
-# coor_A= tuple([random.randint(-10, 10) for i in range(2)])
-# coor_B= tuple([random.randint(-10, 10) for i in range(2)])
-# coor_C= tuple([random.randint(-10, 10) for i in range(2)])
-# triangle = {"A":coor_A, "B": coor_B, "C": coor_C}
-# print(triangle)
-# ########################################
-# my_str = "ENTER SOMETHING"
-# def my_print(my_str):
-#     my_str = "***" + my_str + "***"
-#     return my_str
-# if type(my_str) == str:
-#     my_str = my_print(my_str)
-# print(my_str)
-# ########################################
-my_team = [{"name": "John", "age": 19}, {"name": "Jim", "age": 25}, {"name": "Jany", "age": 28}, {"name": "Janet", "age": 19}]
-my_team_age = []
-my_team_name = []
-asd = []
-for symbol in my_team:
-    my_team_age.append(symbol["age"])
-    my_team_name.append(symbol["name"])
-aver_age = sum(my_team_age) / len(my_team_age)
-print(my_team_age,
-      my_team_name,
-    min(my_team_age))
+import random
+my_rand = [random.randint(0, 101) for i in range(20)]
+print(my_rand)
+########################################
+def r_coordinates():
+    return tuple(random.randint(-10, 10) for i in range(2))
+triangle = {"A":r_coordinates(), "B": r_coordinates(), "C": r_coordinates()}
+print(triangle)
+########################################
+my_str = "I AM STRING!"
+def my_print(my_str):
+    my_str = "***" + my_str + "***"
+    return my_str
+if type(my_str) == str:
+    my_str = my_print(my_str)
+print(my_str)
+#######################################
+persons = [{"name": "John", "age": 19},
+           {"name": "Jimmy", "age": 25},
+           {"name": "Jany", "age": 28},
+           {"name": "Janet", "age": 19}]
+youngest_age = min(person["age"] for person in persons)
+youngest_person = [person["name"] for person in persons if person["age"] == youngest_age]
+longest_name = max(len(person["name"]) for person in persons)
+long_name_person = [person["name"] for person in persons if len(person["name"]) == longest_name]
+print(youngest_person, long_name_person)
+########################################
 
-print(my_team)
+my_dict_1 = {'key1': 0, "key2": 2, "key3": 5, "key4": 22}
+my_dict_2 = {'key1': 0, "key_2": 4, "key3": 6, "key_4": 22}
+my_list_common_key = []
+# A)
+for key in my_dict_1.keys() and my_dict_2.keys():
+    if key in my_dict_1.keys() and my_dict_2.keys():
+        my_list_common_key.append(key)
+print(my_list_common_key)
+# B)
+my_list_1dict_keys = list(set(my_dict_1).difference(set(my_dict_2)))
+print(my_list_1dict_keys)
+# C)
+new_dict = {key: my_dict_1[key] for key in my_list_1dict_keys}
+print(new_dict)
+# D).1
+one_time_key1 = {key: my_dict_1[key] for key in list(set(my_dict_1).difference(set(my_dict_2)))}
+one_time_key2 = {key: my_dict_2[key] for key in list(set(my_dict_2).difference(set(my_dict_1)))}
+new_dict_one_time = {**one_time_key1, **one_time_key2}
+print(new_dict_one_time)
+# D).2
+common_key_dict = {key: [my_dict_1[key], my_dict_2[key]] for key in my_list_common_key}
+print(common_key_dict)
