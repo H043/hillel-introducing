e79384b39c32137700e0f1655a418be11c2d0be0
H043 <74686459+H043@users.noreply.github.com>
2020-12-20 17:25:12 +0200

    home task 7 finally

--- home_task_8.py
+++ home_task_8.py
@@ -1,4 +1,12 @@
 import os
 
-list_dir = os.listdir(".")
-print(list_dir)
+domain_list = []
+def open_domain_file():
+    with open("domains.txt", "r") as file:
+        domain_list = []
+        for line in file.readlines():
+            domain_list.append(line.replace(".", "").strip())
+    return domain_list
+print(open_domain_file())
+
+
